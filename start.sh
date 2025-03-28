28c8a601c0e68b7f48e9a75ddbca633f39427650
Arunachalam <arunabimanyu7@gmail.com>
2025-03-28 05:09:50 +0400

    Updated the config

--- start.sh
+++ start.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
-
 set -e
 
-echo "Enabling IP forwarding..."
-sysctl -w net.ipv4.ip_forward=1
-sysctl -w net.ipv6.conf.all.forwarding=1
+# Enable IP forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
+echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
 
-echo "Setting up NAT64 interface..."
+# Setup NAT64 TUN
 tayga --mktun || true
 ip link set nat64 up
 ip addr add 192.0.2.1 dev nat64 || true
-ip addr add fd00::1 dev nat64 || true
-ip route add 64:ff9b::/96 dev nat64 || true
+ip addr add 2001:db8:1::1 dev nat64 || true
+ip -6 route add 64:ff9b::/96 dev nat64 || true
+iptables -t nat -A POSTROUTING -s 192.0.2.0/24 -o ens6 -j MASQUERADE
 
-echo "Setting up iptables..."
-iptables -t nat -A POSTROUTING -s 192.0.2.0/24 -j MASQUERADE
-
-echo "Starting Unbound DNS64 server..."
+# Start Unbound + Tayga
 service unbound restart
-
-echo "Starting Tayga..."
 tayga &
 
+# Log output
 touch /var/log/unbound.log
+chmod 644 /var/log/unbound.log
 tail -F /var/log/unbound.log
